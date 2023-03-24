eff7702198cc84248f91557553c60fb2f92fbe5f
Julian Gomez <jgomez109@unab.edu.co>
2023-03-24 12:59:29 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  imports: [CommonModule],
 })
-export class AppComponent {
-  title = 'binary-image-area';
-}
+export class AppComponent {}
