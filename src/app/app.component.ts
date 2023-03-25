e1ade77dc74437c4b4d3dbb805c35ed69dd2a822
Julian Gomez <jgomez109@unab.edu.co>
2023-03-25 12:25:10 -0500

    Add loading component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { AfterViewInit, Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { LoadingComponent } from "@app/components/loading/loading.component";
 import { ThemeButtonComponent } from "@app/components/theme-button/theme-button.component";
 import { ThemeService } from "@app/services/theme.service";
 
@@ -8,7 +9,7 @@ import { ThemeService } from "@app/services/theme.service";
   selector: "app-root",
   templateUrl: "./app.component.html",
   standalone: true,
-  imports: [CommonModule, RouterModule, ThemeButtonComponent],
+  imports: [CommonModule, RouterModule, ThemeButtonComponent, LoadingComponent],
 })
 export class AppComponent implements AfterViewInit {
   constructor(private themService: ThemeService) {}
--- src/app/components/loading/loading.component.ts
+++ src/app/components/loading/loading.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ProgressBarComponent } from "@app/components/progress-bar/progress-bar.component";
 
 @Component({
   selector: "app-loading",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProgressBarComponent],
   templateUrl: "./loading.component.html",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoadingComponent {}
+export class LoadingComponent {
+  progressText = "Loading...";
+}
