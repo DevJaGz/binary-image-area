6dfa299baadd0373c831abde96d7a5902f9b2312
Julian Gomez <jgomez109@unab.edu.co>
2023-03-24 20:21:06 -0500

    Create image loader component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ImageLoaderComponent } from '@app/components/image-loader/image-loader.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ImageLoaderComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
